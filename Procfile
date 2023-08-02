7a4b368c29aae3eda95e5459f5e802aaf7735816
james okuku <jamesokuku@gmail.com>
2023-08-02 14:27:02 +0300

    import config

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn training.wsgi
+release: python manage.py migrate
+web: gunicorn training.wsgi --log-file -
--- training/training/settings.py
+++ training/training/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import sys
+import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-iq_n4-kk2=n$ue!dww(*%d+(%=9tjcxwq*nc)g6&2vn4h--h0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sbc-training-b5ba2577f757.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -46,8 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,8 +88,8 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'jemora',
         'HOST': 'localhost',
-        'PORT': '',
-        # 'PORT': '5432'  you can leave the port blank since tihs is the default port
+        # 'PORT': '',
+        'PORT': '5432' # you can leave the port blank since this is the default port
     }
 }
 
@@ -127,14 +128,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
