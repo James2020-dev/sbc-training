2daa15853babd6cbc9e6b4f1097d735c4c70ebef
james okuku <jamesokuku@gmail.com>
2023-08-05 07:21:10 +0300

    static_root file added

--- training/training/settings.py
+++ training/training/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT='staticfiles'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
